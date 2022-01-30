e4a00365758b9f46270f9777ba13d83a90b68fbc
sadarshgowda <sadarshgowda@gmail.com>
2022-01-30 13:02:10 +0530

    reverse each work from given string

---
+++ src/main/java/org/example/string/ReverseEachStringFromWords.java
@@ -0,0 +1,30 @@
+package org.example.string;
+
+public class ReverseEachStringFromWords {
+    public static void main(String[] args) {
+        String s = "God Ding";
+        System.out.println(reverseWords(s)); //"doG gnid"
+    }
+
+    public static String reverseWords(String s) {
+        char[] charArray = s.toCharArray();
+        String result = "";
+        for(int i =0;i<charArray.length; i++){
+            int start = i;
+            while( i<charArray.length && charArray[i] != ' '){
+                i++;
+        }
+        int end =i-1;
+
+        while(end>=start){
+            result = result + charArray[end];
+            end--;
+        }
+        if(i<charArray.length){
+            result += " ";
+        }
+
+    }
+        return result;
+}
+}
