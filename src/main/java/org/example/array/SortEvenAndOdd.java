67fa177a71d994eb2013af873dc6098c3387b1a1
sadarshgowda <sadarshgowda@gmail.com>
2022-02-06 18:24:53 +0530

    logic to sort even and odd elements

---
+++ src/main/java/org/example/array/SortEvenAndOdd.java
@@ -0,0 +1,41 @@
+package org.example.array;
+
+public class SortEvenAndOdd {
+    public static void main(String[] args) {
+        int[] arr = {1,3};
+
+        int[] result = sortEvenAndOdd(arr);
+
+        for(int item : result){
+            System.out.print(item + " ");
+        }
+
+    }
+
+    public static int[] sortEvenAndOdd(int[] nums){
+        int left = 0;
+        int right = nums.length -1;
+
+        while(left<right){
+            while(left != nums.length && nums[left]%2 == 0 ){
+                left++;
+            }
+
+            System.out.println(left);
+
+            while(right != 0 && nums[right]%2 != 0 ){
+                right--;
+            }
+
+            if(left<right ){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+
+            left++;
+            right--;
+        }
+        return nums;
+    }
+}
