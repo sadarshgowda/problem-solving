224a2d74e1bf1f89894f4fb306e623ee750d36ef
sadarshgowda <sadarshgowda@gmail.com>
2022-05-29 17:40:56 +0530

    logic to reverse string

--- src/main/java/org/example/string/ReverseString.java
+++ src/main/java/org/example/string/ReverseString.java
@@ -2,11 +2,25 @@ package org.example.string;
 
 public class ReverseString {
     public static void main(String[] args) {
-        String str = "My name is Adarsh";
-        String result2 = reverseEachCharacter(str);
+        String str = "Adarsh";
+         System.out.println( "the reversed string is " + reverseEachCharacter(str));
     }
 
     public static String reverseEachCharacter(String str){
-        return str;
+        char[] arr = str.toCharArray();
+        int start = 0;
+        int end = str.length()-1;
+
+        while (start<end){
+            char temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+
+            start++;
+            end--;
+        }
+
+
+        return String.valueOf(arr);
     }
 }
