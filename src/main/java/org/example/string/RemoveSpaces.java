cc9b39271a7e6d05cc9921c53768c1f317fb6dc5
sadarshgowda <sadarshgowda@gmail.com>
2022-06-08 09:20:46 +0530

    logic to remove spaces from given string

--- src/main/java/org/example/string/RemoveSpaces.java
+++ src/main/java/org/example/string/RemoveSpaces.java
@@ -3,13 +3,13 @@ package org.example.string;
 public class RemoveSpaces {
     public static void main(String[] args) {
         char str[] = "g eeks for ge eeks ".toCharArray();
-        System.out.println(removeSpacesApproach1(str));
+        System.out.println(removeSpacesApproach1(str)); //geeksforgeeekseeks
         //System.out.println(String.valueOf(str).subSequence(0, i));
 
     }
 
-    static String removeSpacesApproach1(char []str)
-    {
+    public static String removeSpacesApproach1(char[] str) {
+
        int count = 0;
        for(int i=0; i< str.length; i++){
            if(str[i] != ' '){
